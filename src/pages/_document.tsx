a3c98c8864bdb940495a2b26a0dcfd73981df1c8
Julius <juliusagung123@gmail.com>
2024-06-18 09:32:16 +0700

    feat: add favicon & meta property on head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang={"en"}>
-      <Head />
+      <Head >
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="sleepless ai world" />
+        <meta property="og:description" content="Sleepless AI emerges as a groundbreaking Web3+AI gaming platform, ingeniously blending artificial intelligence and blockchain technology. At its core, Sleepless AI aims to revolutionize the gaming industry with its unique approach and the extensive expertise of its team." />
+        <meta property="og:image" content="/alita-ai-logo.png" />
+        <meta property="og:url" content="https://sleeplessaiworld.org" />
+      </Head>
       <body>
         <Main />
         <NextScript />
