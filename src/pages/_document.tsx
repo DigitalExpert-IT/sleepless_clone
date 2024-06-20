a18bc730cd95fc8e9404315756287e577cd578cf
Julius <juliusagung123@gmail.com>
2024-06-20 19:42:43 +0700

    fix: head title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="sleepless ai world" />
+        <meta property="og:title" content="SLEEPLESS AI WORLD" />
         <meta property="og:description" content="Sleepless AI emerges as a groundbreaking Web3+AI gaming platform, ingeniously blending artificial intelligence and blockchain technology. At its core, Sleepless AI aims to revolutionize the gaming industry with its unique approach and the extensive expertise of its team." />
         <meta property="og:image" content="https://sleeplessaiworld.org/purple-girl.png" />
         <meta property="og:url" content="https://sleeplessaiworld.org" />
